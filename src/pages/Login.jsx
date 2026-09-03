ce15d0c3c3e1e66271115a15918da5d1d103655a
frankwsb <frankwsb@uninove.br>
2026-09-03 18:21:58 -0300

    feat: login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,8 @@
 import logo from "../assets/logo.svg"
 import styles from "./Login.module.css"
+import Button from "../components/Button"
+import Input from "../components/Input"
+import Checkbox from "../components/Checkbox"
 
 const Login = () => {
   return (
@@ -8,7 +11,40 @@ const Login = () => {
         <img src={logo} alt="Logotipo Folium" />
         <h1 className={styles.title}>Folium</h1>
         <p>Acompanhe a sua jornada de leitura</p>
-        <button className="button-primary">Entrar</button>
+        <div className={styles.card}>
+          <div className={styles.cardContent}>
+            <form className={styles.form}>
+              <h2>Bem-vindo de volta</h2>
+
+              <div>
+                <Input
+                  id="email"
+                  label="E-mail"
+                  type="email"
+                  placeholder="Digite o seu e-mail"
+                />
+                <Input
+                  id="password"
+                  label="Senha"
+                  type="password"
+                  placeholder="Digite a sua senha"
+                />
+
+                <div className={styles.rememberMe}>
+                  <Checkbox id="rememberMe" label="Lembrar-me" />
+                  <a href="#">Esqueceu a senha?</a>
+                </div>
+
+                <Button title="Entrar" color="primary" />
+              </div>
+
+              <p>
+                Não possui uma conta?
+                <a href="#">Criar uma conta</a>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
     </header>
   )
