30ea23d81baf443898da84c62d3ce854cfe4a8c9
frankwsb <frankwsb@uninove.br>
2026-09-03 12:31:00 -0300

    chore: material icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "material-icons/iconfont/material-icons.css"
 
 createRoot(document.getElementById('root')).render(<App />)
