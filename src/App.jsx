0a7f80309479018fd9710a0b5d50c12569973119
frankwsb <frankwsb@uninove.br>
2026-09-04 17:10:40 -0300

    feat: home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router"
 import './App.css'
 import Login from "./pages/Login"
+import Home from "./pages/Home"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
