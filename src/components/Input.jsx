af6aa81b14cd18c4ac9ca5116de6851964e9b217
frankwsb <frankwsb@uninove.br>
2026-09-04 17:13:58 -0300

    refactor: adding prepend icon and className props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styles from "./Input.module.css"
 
-const Input = ({ id, label, type, ...props }) => {
+const Input = ({ id, label, type, prependIcon, className, ...props }) => {
   const [isVisible, setIsVisible] = React.useState(false)
   const [typeInput, setTypeInput] = React.useState(() =>
     type === "password" ? "password" : "text",
@@ -18,14 +18,16 @@ const Input = ({ id, label, type, ...props }) => {
   }
 
   return (
-    <div className={styles.wrapper}>
+    <div className={className}>
       <label className={styles.label} htmlFor={id}>
         {label}
       </label>
       <div className={styles.content}>
+        {prependIcon && <span className={`material-icons-outlined ${styles.prependIcon}`}>{prependIcon}</span>}
         <input className={styles.input} id={id} type={typeInput} {...props} />
         {type === "password" && (
           <button
+            type="button"
             className={styles.passwordButton}
             onClick={handleIsVisibleIcon}
           >
