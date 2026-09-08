fbd6b5757bc4a5395b002a85b9825fdcf7d2f0e5
frankwsb <frankwsb@uninove.br>
2026-09-08 19:37:07 -0300

    feat: using button icon

--- src/components/home/HomeProfile.jsx
+++ src/components/home/HomeProfile.jsx
@@ -1,10 +1,12 @@
 import profile from "../../assets/profile.jpg"
+import ButtonIcon from "../ButtonIcon"
 import styles from './HomeProfile.module.css'
 
 const HomeProfile = () => {
   return (
     <div className={styles.profileContainer}>
       <img className={styles.profile} src={profile} alt="Sua foto de perfil" />
+      <ButtonIcon className={styles.profileMenuButton} icon="menu" />
     </div>
   )
 }
