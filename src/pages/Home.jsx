b4bfe5576cd77018b0215f7b12b49728a8af53ba
frankwsb <frankwsb@uninove.br>
2026-09-24 12:05:15 -0300

    refactor: change book statuses

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,28 +21,28 @@ const Home = () => {
       title: "The Midnight Library",
       author: "Matt Haig",
       status: "1",
-      stars: 5
+      rating: 5
     },
     {
       id: 2,
       title: "Atomic Habits",
       author: "James Clear",
       status: "0",
-      stars: null
+      rating: null
     },
     {
       id: 3,
       title: "Project Hail Mary",
       author: "Andy Weir",
-      status: "0",
-      stars: null
+      status: "2",
+      rating: null
     },
     {
       id: 4,
       title: "The Silent Patient",
       author: "Alex Michaelides",
-      status: "2",
-      stars: 4
+      status: "1",
+      rating: 4
     },
   ]
 
